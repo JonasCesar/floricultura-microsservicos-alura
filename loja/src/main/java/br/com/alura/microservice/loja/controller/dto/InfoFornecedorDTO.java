0152805c0f619d42aea3ac3b3f07be9286516544
Jonas César Silva <jonascesar.silva@dxc.com>
2022-10-27 12:08:09 -0300

    Usando o RestTemplate para buscar informações do fornecedor

---
+++ loja/src/main/java/br/com/alura/microservice/loja/controller/dto/InfoFornecedorDTO.java
@@ -0,0 +1,15 @@
+package br.com.alura.microservice.loja.controller.dto;
+
+public class InfoFornecedorDTO {
+	
+	private String Endereco;
+
+	public String getEndereco() {
+		return Endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		Endereco = endereco;
+	}
+
+}
